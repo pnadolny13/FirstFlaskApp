25ac223a79fe5f3fe90162a7779872f2970bbab4
pnadolny13 <pnadolny@deloitte.com>
2018-01-05 20:18:10 -0500

    Update to accept 100days form log inputs

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, request, redirect
+import datetime
+import os
 app = Flask(__name__)
- 
-email_addresses = []
+
 @app.route("/")
 def hello():
     thisAuthor = "Pat"
@@ -12,14 +13,37 @@ def hello():
  
 @app.route('/signup', methods = ['POST'])
 def signup():
-    email = request.form['email']
-    email_addresses.append(email)
-    print(emails)
+    global day
+    day = request.form['day']
+    global today
+    today = request.form['today']
+    global thoughts
+    thoughts = request.form['thoughts']
+    global links
+    links = request.form['links']    
+    global date
+    date = datetime.datetime.now().strftime("%m %B, %Y")
+    print(day + today + thoughts + links + date)
+    
+    #open log file and write to it
+    file = "C:\\Users\\pnadolny\\Documents\\Personal_Development\\GitHub\\100-days-of-code\\log - Copy.md"
+    content = open(file,"r")
+    for line in content:
+        print (line)    
+    content.close()
+    content = open (file, "a")
+    content.write("\n \n \n ### Day " + day + ": " + date + "\n \n")
+    content.write("**Today\'s Progress**: " + today + "\n \n")    
+    content.write("**Thoughts** " + thoughts + "\n \n")
+    content.write("**Link(s) to work:** " + links + "\n")
+    content.close() 
+    
     return redirect('/')    
 
-@app.route('/emails.html')
-def emails():
-    return render_template('emails.html', author='Pat', emails=email_addresses)
+@app.route('/logs.html')
+def logs():
+    print(day)
+    return render_template('logs.html', author='Pat', day=day , date=date , today=today , thoughts=thoughts , links=links)
 
 
 if __name__ == "__main__":
