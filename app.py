877c83b7e7b94c0384c368a88c596fa1fc7ce5d0
pnadolny13 <pnadolny@deloitte.com>
2018-02-05 19:01:32 -0500

    Added SQL Alchemy
    doesnt work for delete yet but inserts work.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-
+#from flask.ext.cqlalchemy import CQLAlchemy
+import csv
 import datetime
 app = Flask(__name__)
-Bootstrap(app)
+bootstrap = Bootstrap(app)
 
 warning = ""
 file = "C:\\Users\\pnadolny\\Documents\\Personal_Development\\GitHub\\100-days-of-code\\log.md"
@@ -15,12 +17,53 @@ recent = ""
 recentLine = 0
 
 
+
+
+############ CQL AlCHEMY #######################
+#app.config['CASSANDRA_HOSTS'] = ['127.0.0.1']
+#app.config['CASSANDRA_KEYSPACE'] = "logsDB"
+#db = CQLAlchemy(app)
+
+
+#class User(db.Model):
+#    day = db.columns.Text(primary_key=True)
+#    log = db.columns.Text(required=False)
+
+
+###############   SQL AlCHEMY   ##################
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dbs\\log.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Logs(db.Model):
+    day = db.Column(db.String, primary_key=True)
+    logContent = db.Column(db.String)  
+    
+    def __repr__(self):
+        return '%r' % self.logContent
+
+
 #######    VIEWS  ################
+
+@app.route("/test")
+def test():
+    response = "{ \"response\" : \"pat\" }"
+    print (jsonify(response))
+    return (jsonify(response))
+
 
 @app.route("/")
 def homeView():
     prepForInputPage()
-    log = open(file,"r")
+    log = open(file,"r")    
     return render_template('index.html', warning=warning, log=log )
 
 @app.route('/input')
@@ -65,20 +108,30 @@ def delete():
     # call input with no inputs to load in variables if needed
     global recent       
     if date in recent:
-        logsR = open(file,"rb")
+        logsR = open(file,"r")
         lines = logsR.readlines()
         logsR.close()
-        logsW = open(file,"wb")            
+        
+        with open(file, 'w') as outfile:
+            writer = csv.writer(outfile)        
+        
+        logsW = open(file,"w", newline= '')          
         count = 1;
         for line in lines:
             if count == (recentLine - 2) or count == (recentLine - 1) or count >= recentLine:
                 #deleteLine
                 print("Delete: " + str(line))
             else:
+                writer.writerow(line)
                 logsW.write(line)
             count += 1
         recent = ""
         logsW.close()
+        
+        # delete from sql db
+#        log = Logs(day=str(today), logContent=today)
+#        db.session.delete(Logs.delete())
+#        db.session.commit()
     return redirect('/')
 
 
@@ -100,7 +153,8 @@ def inputs():
             warning = "ENTRY FOR TODAY ALREADY EXISTS!!!"
             return render_template('input.html', warning=warning, recent=recent)
     content.close()
-
+    # logContent = "### Day " + str(day) + ": " + str(date) + "**Today\'s Progress**: " + str(today)
+    # + "**Thoughts** " + thoughts + "**Link(s) to work:** " + links;
     # day doesnt exist, go ahead and update now
     content = open (file, "a")
     content.write("\n\n\n### Day " + str(day) + ": " + date + "\n\n")
@@ -108,6 +162,12 @@ def inputs():
     content.write("**Thoughts** " + thoughts + "\n\n")
     content.write("**Link(s) to work:** " + links)
     content.close()
+    
+    # write to sql db
+    log = Logs(day=str(day), logContent=today)
+    db.session.add(log)
+    db.session.commit()
+
     
     return redirect('/')    
 
@@ -131,6 +191,7 @@ def prepForInputPage():
             recent = "Day " + lastDay + ": " + date;
     logs.close();
     print (recentLine)
+        
 
 if __name__ == "__main__":
     app.run()
